d12b39234f2a447af63fa6e7145a3c0bc25c4e45
unknown <ts0963187279@gmail.com>
2018-04-02 19:26:19 +0800

    5.LongestPalindromicSubstring's runtime beats 44.92% of cpp submissions when using Manacher's algorithm

--- 5.LongestPalindromicSubstring.cpp
+++ 5.LongestPalindromicSubstring.cpp
@@ -2,26 +2,30 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int max = 0;
-        string ans;
+        string T,ans;
+        int longest = 0 , longestIndex , left , right;
         for(int i = 0 ; i < s.size() ; i++){
-            string str = "";
-            for(int j = 0 ; j < s.size() - i ; j ++){
-                bool cycle = true;
-                str += s[i + j];
-                if(s[i] == s[i + j]){
-                    for(int k = 0 ; k < str.size() / 2 ; k++){
-                        if(str[k] != str[str.size() - k - 1]){
-                            cycle = false;
-                            break;
-                        }
-                    }
-                    if(cycle && str.size() > max){
-                        max = str.size();
-                        ans = str; 
-                    }  
-                }
+            T += "#";
+            T += s[i];
+        }
+        T += "#";
+        vector<int> P(T.size(),0);
+        for(int i = 0 ; i < T.size() ; i++){
+            left = i - 1;
+            right = i + 1;
+            while(T[left] == T[right] && left >= 0 && right < T.size()){
+                P[i] ++;
+                left --;
+                right ++;
             }
+            if(P[i] > longest){
+                longest = P[i];
+                longestIndex = i;
+            }
+        }
+        for(int i = 0 ; i < longest * 2; i++){
+            if(T[longestIndex - longest + i] != '#')
+                ans += T[longestIndex - longest + i];
         }
         return ans;
     }
