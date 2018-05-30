df06c17f2b40e6ef85c5ca3f74b3de2ff6f4c4f0
RS Wang <ts0963187279@gmail.com>
2018-05-30 12:44:25 +0800

    771. Jewels and Stones AC

---
+++ 771. Jewels and Stones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int c = 0;
+        for(int i=0;i<S.size();i++){
+            for(int j=0;j<J.size();j++){
+                if(S[i]==J[j])
+                    c++;
+            }
+        }
+        return c;
+    }
+};
